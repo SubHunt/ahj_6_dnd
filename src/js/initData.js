3092b2bba4344cabee2f5c49c54f38b58f8c5911
SubHunt <phaeton.kz@gmail.com>
2024-04-26 12:26:16 +0300

    Initial

---
+++ src/js/initData.js
@@ -0,0 +1,7 @@
+export default function initData() {
+  return {
+    todo: ['Create GitHub repo', 'Testing local', 'Publish GitHub Pages'],
+    inProgress: ['Connect Appveyor', 'Add Badge Appveyor', 'Edit README', 'Save all changes'],
+    done: ['Push GitHub', 'Enjoy'],
+  };
+}
